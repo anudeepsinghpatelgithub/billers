4cb48da13859fc8c72989e096bd17333afcbd24d
anudeeppatel <anudeepsinghpatel@gmail.com>
2019-12-05 15:56:47 +0530

    added all cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const cors = require('cors')
 
 
 const app = express();
@@ -10,6 +11,8 @@ app.use(express.json({extended:false}))
 
 
 const PORT = process.env.PORT || 5000;
+
+app.use(cors())
 
 app.get('/',(req,res)=>{
     res.send({msg:'api running'});
